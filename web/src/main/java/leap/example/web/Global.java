5a70710b588a2372a37f3d2c23ccaa5cbc5d4f56
Kael <penghj@bingosoft.net>
2016-12-23 15:14:51 +0800

    add web example

--- web/src/main/java/leap/example/web/Global.java
+++ web/src/main/java/leap/example/web/Global.java
@@ -1,7 +1,18 @@
 package leap.example.web;
 
+import leap.lang.logging.Log;
+import leap.lang.logging.LogFactory;
+import leap.web.App;
+
 /**
  * Created by kael on 2016/12/23.
  */
-public class Global {
+public class Global extends App{
+    
+    private static final Log log = LogFactory.get(Global.class);
+    
+    @Override
+    protected void init() throws Throwable {
+        log.info("web app init...");
+    }
 }
--- web/src/main/java/leap/example/web/controller/HomeController.java
+++ web/src/main/java/leap/example/web/controller/HomeController.java
@@ -1,7 +1,13 @@
 package leap.example.web.controller;
 
+import leap.web.action.ControllerBase;
+import leap.web.view.ViewData;
+
 /**
  * Created by kael on 2016/12/23.
  */
-public class HomeController {
+public class HomeController extends ControllerBase {
+    public void index(ViewData vd){
+        vd.put("hello","hello leap!");
+    }
 }
